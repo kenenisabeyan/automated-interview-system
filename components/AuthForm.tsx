167a344e50bada2f3ef1a0956a38e62ea291573a
Kenenisa Beyan <kenenisab05@gmail.com>
2026-05-26 21:59:51 +0300

    fix: remove unnecessary comment in AuthForm component import

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -2,7 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";//
+import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import Image from "next/image";
